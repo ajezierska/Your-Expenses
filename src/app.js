593d8e7cd299b42d097313b227347252da82ffff
Andżelika Jezierska <a.b.jezierska@gmail.com>
2018-11-28 20:15:17 +0100

    create new component - summary our expenses

--- src/app.js
+++ src/app.js
@@ -11,11 +11,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water Bill', amount: 50, createdAt: 15}));
-store.dispatch(addExpense({description: 'Gas Bill',createdAt: 155}));
-store.dispatch(addExpense({description: 'Rent', amount: 589550, createdAt: 15}));
-
-
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
