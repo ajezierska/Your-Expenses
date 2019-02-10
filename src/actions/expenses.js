6d4b8c9e32e066287ad57b1f122c6342a25beb70
ajezierska <a.b.jezierska@gmail.com>
2019-02-10 17:01:45 +0100

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -27,12 +27,18 @@ export const startAddExpense = (expensesData = {}) => {
 }
 
 // ACTIONS - REMOVE_EXPENSE
-export const removeExpense = ({
-  id
-} = {}) => ({
+export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
+
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }))
+    })
+  }
+}
 
 // ACTIONS - EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
